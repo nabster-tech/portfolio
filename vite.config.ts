5113f797598a3157f1d4a1ac40bdc0e914087d1c
Tushar Raj <tushar.raj@t-systems.com>
2025-08-21 14:12:00 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/", // 👈 Add this line
   optimizeDeps: {
-    include: ['react', 'react-dom']
+    include: ["react", "react-dom"],
   },
   server: {
     port: 5173,
-    host: true
-  }
-})
+    host: true,
+  },
+});
